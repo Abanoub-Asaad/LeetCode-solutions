45c7ea9cea218a0ee2a6d61966fbd09359a259ea
Abanoub Asaad <48657780+Abanoub-Asaad@users.noreply.github.com>
2022-02-15 13:50:49 +0200

    Time: 78 ms (44.35%), Space: 33 MB (33.56%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -7,32 +7,19 @@ public:
         vector<int> ans;
         priority_queue<int, vector<int>, greater<int>> pq;
         
-        if(x < arr[0]) 
+        for(int i = 0; i < n; i++)
         {
-            for(int i = 0; i < k; i++)
-                ans.push_back(arr[i]);
-        } 
-        else if(x > arr[n-1]) 
-        {
-            for(int i = n-k; i < n; i++)
-                ans.push_back(arr[i]);
-        } 
-        else 
-        {
-            for(int i = 0; i < n; i++)
+            if(pq.size() != k)
+                pq.push(arr[i]);
+            else 
             {
-                if(pq.size() != k)
-                    pq.push(arr[i]);
-                else 
-                {
-                    if(abs(arr[i] - x) < abs(pq.top() - x))
-                        pq.pop(), pq.push(arr[i]);
-                }
+                if(abs(arr[i] - x) < abs(pq.top() - x))
+                    pq.pop(), pq.push(arr[i]);
             }
-            
-            while(pq.size())
-                ans.push_back(pq.top()), pq.pop();
         }
+            
+        while(pq.size())
+            ans.push_back(pq.top()), pq.pop();
         
         return ans;
     }
