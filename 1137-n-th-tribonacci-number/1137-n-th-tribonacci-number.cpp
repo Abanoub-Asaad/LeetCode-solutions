b59a8f299459bdad74d60eb9d34706675605fd44
Abanoub Asaad <48657780+Abanoub-Asaad@users.noreply.github.com>
2022-02-25 02:52:37 +0200

    Time: 0 ms (100.00%), Space: 5.8 MB (83.30%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        
+        if(!n) return 0;
+        if(n <= 2) return 1;
+        int a = 0, b = 1, c = 1, sum = 0;
+        n-=2;
+        while(n--)
+        {
+            sum = a + b + c;
+            a = b;
+            b = c;
+            c = sum;
+        }
+        
+        return sum;
+    }
+};
+
+
+/*
+    - Recursive
+    - Iterative: Time=O(N), Space=O(1)
+    - DP (Top-Bottom)
+    - DP (Bottom-Up)
+
+
+*/
