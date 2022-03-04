cf8fd81c3654329195186fe64d0ba556a315ec64
Abanoub Asaad <48657780+Abanoub-Asaad@users.noreply.github.com>
2022-03-04 20:09:58 +0200

    Time: 4 ms (92.15%), Space: 6.1 MB (75.19%) - LeetHub

--- 799-champagne-tower/799-champagne-tower.cpp
+++ 799-champagne-tower/799-champagne-tower.cpp
@@ -4,7 +4,7 @@ public:
         
         double dp[101][101] = {0.0};
         dp[0][0] = poured; 
-        for(int i = 0; i < 100; i++) {
+        for(int i = 0; i <= query_row; i++) {
             for(int j = 0; j <= i; j++) {
                 if(dp[i][j] >= 1) {
                     dp[i+1][j] += (dp[i][j]-1)/2.0;
@@ -18,25 +18,5 @@ public:
 };
 
 /*
-    When I pour one cup 
-    When I pour 2 cups
-    When I pour 3 cups
-    When I pour 4 cups
-    When I pour 5 cups
-    
-    1 cup ->  first row-> 1
-    2 cups->  first row-> 1, second row->.5, .5
-    3 cups -> first row-> 1, second row-> 1, 1
-    4 cups -> first row-> 1, second row-> 1, 1, third row->.25, .5, .25    
-    5 cups -> first row-> 1, second row-> 1, 2, third row->.5, 1, .5
-    
-                   1
-                  ---
-                  1   1
-                 --- --- 
-              .5    1    .5
-              --   ---    --
-            -- -- -- -- -- --
-    
-    I wanna know, each row needs how many cups to be filled
+   Time: O(query_row ^ 2)
 */
