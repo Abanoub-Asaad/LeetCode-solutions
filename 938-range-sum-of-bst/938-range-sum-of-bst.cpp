c796e9cc0bea57097995f8e0588a20dc948b1ca6
Abanoub Asaad <48657780+Abanoub-Asaad@users.noreply.github.com>
2022-03-11 13:01:23 +0200

    Time: 112 ms (97.97%), Space: 64.7 MB (19.09%) - LeetHub

--- 938-range-sum-of-bst/938-range-sum-of-bst.cpp
+++ 938-range-sum-of-bst/938-range-sum-of-bst.cpp
@@ -12,6 +12,28 @@
 class Solution {
 public:
     
+    //Time: O(M), M is all the nodes in the range [low, high]
+    //Space: O(H), H is the height of the tree
+    
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        if(!root)
+            return 0;
+        if(root->val > high)
+            return rangeSumBST(root->left, low, high);
+        if(root->val < low)
+            return rangeSumBST(root->right, low, high);
+        return root->val + rangeSumBST(root->left, low, high) + rangeSumBST(root->right, low, high);
+    }
+};
+
+/*------------------------------------------------------------------------------
+------------ Time: O(N), N is all the nodes
+------------ Space: O(H), H is the height of the tree
+------------ Here we are not talking an advantage of that the tree is BST
+/*------------------------------------------------------------------------------
+class Solution {
+public:
+    
     void dfs(TreeNode* root, int l, int h, int &sum) {
         
         // base case
@@ -31,13 +53,4 @@ public:
         return sum;
     }
 };
-
-/*
-    Iterate over the whole tree, when I see node's val in the range 
-     I'll add it to the sum
-     
-    DFS 
-    
-    Time: O(N)
-    Space: O(H)
-*/
+---------------------------------------------------------*/
