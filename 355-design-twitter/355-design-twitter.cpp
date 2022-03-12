6d6434364a262e57f496850443b88c3fb8f1001c
Abanoub Asaad <48657780+Abanoub-Asaad@users.noreply.github.com>
2022-03-12 15:45:40 +0200

    Time: 6 ms (20.80%), Space: 7.2 MB (46.73%) - LeetHub

---
+++ 355-design-twitter/355-design-twitter.cpp
@@ -0,0 +1,93 @@
+class Twitter {
+public:
+    
+    /*
+    [postTweet", "getNewsFeed", "follow", "postTweet", "getNewsFeed", "unfollow", "getNewsFeed"]
+                                                                                     ^
+    [[1, 5], [1], [1, 2], [2, 6], [1], [1, 2], [1]]
+                                                ^
+      
+    hash map     followingPeople
+    user  tweet  user
+    1  -> 5,3       0
+    2  -> 6         0
+    
+    Make a hash map of vector
+    key = userId, value'vector' = tweets
+        
+    Assume we have a priority queue <= 10
+    "maxHeap" to retrieve the most recent tweets
+    
+    If I followed someone
+        I would insert his tweets into the pq //till the size become 10
+    If I unfollowed someone
+        I would remove his tweets from the pq
+        
+    HashMap, user->tweets, 
+    HashMap, user -> hashSet
+    MaxHeap
+        
+    */
+    
+    unordered_map<int, vector<int>> userTweets;
+    unordered_map<int, int> timeTweet;
+    unordered_map<int, unordered_set<int>> userFollowings;
+    int curTime = 1;
+    
+    Twitter() {
+        
+    }
+    
+    void postTweet(int userId, int tweetId) {
+        userTweets[userId].push_back(curTime);
+        timeTweet[curTime] = tweetId;
+        ++curTime;
+    }
+    
+    vector<int> getNewsFeed(int userId) {
+        
+        priority_queue<int> newsFeedPQ;
+        
+        //get the cur user's tweets
+        for(auto tweet : userTweets[userId]) {
+            newsFeedPQ.push(tweet);
+        }
+        
+        //push the followings' tweets
+        for(auto user = userFollowings[userId].begin(); user != userFollowings[userId].end(); user++) 
+        {
+            int followeeId = *user;
+            for(auto tweet : userTweets[followeeId]) {
+                newsFeedPQ.push(tweet);
+             }
+        }
+        
+        vector<int> newFeedList;
+        while(newsFeedPQ.size()) 
+        {
+            if(newFeedList.size() == 10)
+                break;
+            newFeedList.push_back(timeTweet[newsFeedPQ.top()]);
+            newsFeedPQ.pop();
+        }
+          
+        return newFeedList;
+    }
+    
+    void follow(int followerId, int followeeId) {
+        userFollowings[followerId].insert(followeeId);
+    }
+    
+    void unfollow(int followerId, int followeeId) {
+        userFollowings[followerId].erase(followeeId);
+    }
+};
+
+/**
+ * Your Twitter object will be instantiated and called as such:
+ * Twitter* obj = new Twitter();
+ * obj->postTweet(userId,tweetId);
+ * vector<int> param_2 = obj->getNewsFeed(userId);
+ * obj->follow(followerId,followeeId);
+ * obj->unfollow(followerId,followeeId);
+ */
