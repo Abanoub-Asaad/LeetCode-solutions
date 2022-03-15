2a7ae966a384da0005977cbfafcb1f6bd9d409d1
Abanoub Asaad <48657780+Abanoub-Asaad@users.noreply.github.com>
2022-03-15 19:43:04 +0200

    Time: 3 ms (59.21%), Space: 6 MB (32.38%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.cpp
+++ 7-reverse-integer/7-reverse-integer.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
+    
+    //Time: O(log x), Space: O(1)
+    
     int reverse(int x) {
-        
         long long ans = 0;
         while(x) {
             ans = (ans*10) + (x%10);
             x /= 10;
         }
-        
         return (ans < INT_MIN || ans > INT_MAX) ? 0 : ans;
     }
 };
