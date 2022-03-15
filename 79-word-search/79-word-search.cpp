a9d7b7ceaf9c02edec7f999b656337ab91c9a661
Abanoub Asaad <48657780+Abanoub-Asaad@users.noreply.github.com>
2022-03-15 21:32:28 +0200

    Time: 690 ms (38.15%), Space: 7.9 MB (75.19%) - LeetHub

--- 79-word-search/79-word-search.cpp
+++ 79-word-search/79-word-search.cpp
@@ -25,7 +25,7 @@ public:
     bool exist(vector<vector<char>>& board, string word) {
         for(int i = 0; i < board.size(); i++) {
             for(int j = 0; j < board[0].size(); j++) {
-                if(word[0] == board[i][j] && dfs(board, word, i, j, 0))
+                if(dfs(board, word, i, j, 0))
                     return true;
              }
         }
