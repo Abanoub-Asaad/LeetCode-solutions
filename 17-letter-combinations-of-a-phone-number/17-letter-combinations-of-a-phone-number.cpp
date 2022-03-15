e37f08ace186e54363a726d1cc8ca6049a568f76
Abanoub Asaad <48657780+Abanoub-Asaad@users.noreply.github.com>
2022-03-16 00:01:42 +0200

    Time: 3 ms (36.65%), Space: 6.6 MB (34.93%) - LeetHub

---
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    
+    unordered_map<char, string> map;
+    vector<string> ans;
+    
+    void backTrack(string digits, int i, string curStr) {
+        if(curStr.size() == digits.size())
+        {   
+            ans.push_back(curStr);
+            return;
+        }
+        for(auto c : map[digits[i]])
+            backTrack(digits, i+1, curStr + c);
+    }
+    
+    vector<string> letterCombinations(string digits) {
+
+        map['2'] = "abc";
+        map['3'] = "def";
+        map['4'] = "ghi";
+        map['5'] = "jkl";
+        map['6'] = "mno";
+        map['7'] = "pqrs";
+        map['8'] = "tuv";
+        map['9'] = "wxyz";
+     
+        if(digits.size())
+            backTrack(digits, 0, "");
+        return ans;
+    }
+};
+
+
+/*
+    Time: O(N * 4^N)
+    
+    2     3
+    abc  def
+    
+    ad, ae, af
+    bd, be, bf
+    cd, ce, cf
+    
+    - Map the digits to the corrsponding characters
+    - Backtrack
+*/
