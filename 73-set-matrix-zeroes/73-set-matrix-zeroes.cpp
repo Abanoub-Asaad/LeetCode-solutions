82fe2ef96deb31148b7c0a6c08d021eec62854df
Abanoub Asaad <48657780+Abanoub-Asaad@users.noreply.github.com>
2022-03-20 14:03:26 +0200

    Time: 20 ms (53.31%), Space: 13.4 MB (11.38%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,32 +1,44 @@
 class Solution {
 public:
     
-    void markZeroes(vector<vector<int>>& matrix, int x, int y) {
+    void markCol(vector<vector<int>>& matrix, int x, int y) {
         int n = matrix.size();
-        int m = matrix[0].size();
-        for(int j = 0; j < y; j++) {
-            matrix[x][j] = 0;
-        }
-        for(int j = y+1; j < m; j++) {
-            matrix[x][j] = 0;
-        }
         for(int i = 0; i < x; i++) {
             matrix[i][y] = 0;
         }
-        for(int i = x+1; i < n; i++) {
+        for(int i = x; i < n; i++) {
             matrix[i][y] = 0;
         }
     }
     
+    void markRow(vector<vector<int>>& matrix, int x, int y) {
+        int m = matrix[0].size();
+        for(int j = 0; j < y; j++) {
+            matrix[x][j] = 0;
+        }
+        for(int j = y; j < m; j++) {
+            matrix[x][j] = 0;
+        }
+    }
+    
     void setZeroes(vector<vector<int>>& matrix) {
-        vector<vector<int>> helperArr = matrix;
+        unordered_map<string, vector<int>> markVect; //row: x,y  col: x,y
         for(int i = 0; i < matrix.size(); i++) {
             for(int j = 0; j < matrix[0].size(); j++) {
-                if(matrix[i][j] == 0)
-                    markZeroes(helperArr, i, j);
+                if(matrix[i][j] == 0) {
+                    string key = to_string(i)+"0"+"0"+to_string(j);
+                    if(!markVect.count(key))
+                        markVect[key] = {i, 0, 0, j};
+                }
             }
         }
-        matrix = helperArr;
+        
+        for(auto i = markVect.begin(); i != markVect.end(); i++) {
+            vector<int> curr = i->second;
+            cout << curr[0] << " " << curr[1] << " " << curr[2] << " " << curr[3] << endl;
+            markRow(matrix, curr[0], curr[1]);
+            markCol(matrix, curr[2], curr[3]);
+        }
     }
 };
 
@@ -38,7 +50,11 @@ public:
     When I see a 0, I will mark all its neighbours as visited or make them zeroes
     
     111
+    101
+    111
+    
+    111
     100
-    100
+    101
     
 */
