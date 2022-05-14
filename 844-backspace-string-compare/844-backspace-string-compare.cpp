adce9b89babc79993917ba26addce6d69d76374d
Abanoub Asaad <48657780+Abanoub-Asaad@users.noreply.github.com>
2022-05-14 12:21:42 +0200

    Time: 0 ms (100.00%), Space: 6.5 MB (21.83%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.cpp
+++ 844-backspace-string-compare/844-backspace-string-compare.cpp
@@ -1,45 +1,37 @@
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
-        int n = s.size();
-        string curS = "";
-        int cnt = 0;
-        for(int i = n-1; i >= 0; i--) {
-            if(s[i] != '#') {
-                if(cnt)
-                    cnt--;
-                else 
-                    curS += s[i];
-            }
-            else 
-                cnt++;
+        
+        stack<char> stackS, stackT;
+        for(auto i : s) {
+            if(i != '#')
+                stackS.push(i);
+            else if(stackS.size())
+                stackS.pop();
+        }
+        for(auto i : t) {
+            if(i != '#')
+                stackT.push(i);
+            else if(stackT.size())
+                stackT.pop();
         }
         
-        n = t.size();
-        string curT = "";
-        cnt = 0;
-        for(int i = n-1; i >= 0; i--) {
-            if(t[i] != '#') {
-                if(cnt)
-                    cnt--;
-                else 
-                    curT += t[i];
-            }
+        //compare the two stacks
+        while(stackS.size() && stackT.size()) {
+            if(stackS.top() == stackT.top()) 
+                stackS.pop(), stackT.pop();
             else 
-                cnt++;
+                return false;
         }
-        
-        return curS == curT;
+        return (stackS.empty() && stackT.empty()) ? true : false;
     }
 };
 
 /*
-    a##c
-    c
-    
-    ca#b##
-    cnt = 2
-    
-
+    First Approach: using stacks
+     Time: O(N), Space: O(N)
+     
+    Second Approach: 
 
 */
+
