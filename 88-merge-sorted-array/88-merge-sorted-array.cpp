23ed6ac6120b89b2f7661a8306cfa766de40ca37
Abanoub Asaad <48657780+Abanoub-Asaad@users.noreply.github.com>
2022-06-07 11:32:37 +0200

    Time: 4 ms (49.07%), Space: 9.1 MB (72.57%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,21 +1,40 @@
+/***********************
+Time: O(N+M)
+Space: O(1)
+************************/
 class Solution {
 public:
-    //--------- Time: O(N+M)
-    //--------- Space: O(1)
-    
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
         
         int i = m + n - 1;
         m--, n--;
         
         while(m >= 0 && n >= 0) {
-            if(nums1[m] > nums2[n])
+            if(nums1[m] > nums2[n]) {
                 nums1[i--] = nums1[m--];
-            else 
+            } else {
                 nums1[i--] = nums2[n--];
+            }   
         }
         while(n >= 0) {
             nums1[i--] = nums2[n--];
         }
     }
 };
+
+
+/***************************************************************************
+ Another Solution
+  Time: O((n+m)log(n+m))
+  Space: O(1)
+****************************************************************************
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        for(int i = m, j = 0; i < n+m, j < n; i++, j++) {
+            nums1[i] = nums2[j];
+        }
+        sort(nums1.begin(), nums1.end());
+    }
+};
+****************************************************************************/
